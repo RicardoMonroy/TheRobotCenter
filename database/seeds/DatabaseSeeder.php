d57a7bf885ab8e1c4881935b2f4c027d38f10cb0
Ricardo Monroy <rmonroy.rodriguez@gmail.com>
2019-06-20 15:31:34 -0500

    TableSeeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(LevelsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(SchoolsTableSeeder::class);
+
     }
 }
